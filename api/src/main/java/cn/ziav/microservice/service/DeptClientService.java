8d9eb4b6fd46e6d52eaeb3675200572bbb376798
Z.avi <zavi.zheng@pkfare.com>
2018-12-12 10:43:32 +0800

    pom update

--- api/src/main/java/cn/ziav/microservice/service/DeptClientService.java
+++ api/src/main/java/cn/ziav/microservice/service/DeptClientService.java
@@ -2,12 +2,11 @@ package cn.ziav.microservice.service;
 
 import cn.ziav.microservice.DeptClientServiceFallbackFactory;
 import cn.ziav.microservice.entity.Dept;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @FeignClient(
     value = "MICROSERVICECLOUD-DEPT",
